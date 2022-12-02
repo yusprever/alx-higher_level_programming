fd9dc3de63f54dc43c4f85518d10846ecedde597
yusprever <kiserotobin12@gamil.com>
2022-12-02 11:06:47 -0800

    hotfix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     argv_count = len(argv)
     operators = ["+", "-", "*", "/"]
-    if argv_count is not 3:
+    if argv_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif sys.argv[2] not in operators:
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        if sys.argv[2] is "+":
+        if sys.argv[2] == "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] is "-":
+        elif sys.argv[2] == "-":
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] is "*":
+        elif sys.argv[2] == "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
+        elif sys.argv[2] == "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
